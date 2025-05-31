894c09794a3b6b59dabef0fa61cbdeb4333f9856
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-05-31 21:59:39 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyATXdsB_HxP3xY60qsZ6kAydC9zdQGDTaU",
+  authDomain: "encyclopedialphabet.firebaseapp.com",
+  projectId: "encyclopedialphabet",
+  storageBucket: "encyclopedialphabet.appspot.com",
+  messagingSenderId: "225712774247",
+  appId: "1:225712774247:android:a5c82fbbcd341b137c08c2"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
